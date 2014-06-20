98d7bccf6aa64f0f3f0ecef55cf4e202ee347ec1
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-20 14:15:24 -0400

    Changed the JQuery event to be on submit, rather than on click.  Accounts for users hitting enter instead of clicking the button.

--- guess.js
+++ guess.js
@@ -1,26 +1,26 @@
 var secretNumber = (Math.floor(Math.random() * 99) + 1);
+var guessTooLow = 'Too low'
+var guessTooHigh = 'Too high'
+var guessCorrect = 'You got it! <a href=file:///Users/murphydbuffalo/Dropbox/launchacademy/guess_the_number_js/guess.html> Play Again </a>'
 
-$('#submit').on('click', function(event) {
+//Call on('submit') on the FORM not the submit button or ID
+//You are submitting a form, not a button
+$('form').on('submit', function(event) {
   event.preventDefault();
 	var $guess = $('#guess').val();
+
+	if($guess < secretNumber){
+		$('.messages p').html(guessTooLow);
+	}
+	else if($guess > secretNumber) {
+		$('.messages p').html(guessTooHigh);
+	}
+	else {
+		$('.messages p').html(guessCorrect);
+	}
 	$('.guesses ul').append('<li>' + $guess + '</li>');
 })
 
-/*function getInput() {
-	prompt('Please guess a number between 1 and 100.');
-}
-
-
-	for(var input = getInput(); input != secretNumber; input = prompt('Guess again!')){
-		if(input < secretNumber){
-			alert('Too low!');
-		}
-		else if(input > secretNumber) {
-			alert('Too high!');
-		}
-	}
-
-alert('You got it!');*/
 
 
 
