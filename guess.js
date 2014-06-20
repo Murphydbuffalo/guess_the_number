9c8b1628f9d9a9ffc1372230a0a7995d64470932
Dan Murphy <murphydbuffalo@gmail.com>
2014-06-20 15:20:37 -0400

    Added validation that user input is between 1 and 100.

--- guess.js
+++ guess.js
@@ -1,13 +1,21 @@
 var secretNumber = (Math.floor(Math.random() * 99) + 1);
-var guessTooLow = 'Too low'
-var guessTooHigh = 'Too high'
+var guessTooLow = 'Too low.'
+var guessTooHigh = 'Too high.'
 var guessCorrect = 'You got it! <a href=file:///Users/murphydbuffalo/Dropbox/launchacademy/guess_the_number_js/guess.html> Play Again </a>'
+var notANumber = 'Please enter an integer between 1 and 100.'
+
+/*function validNumberCheck(string, message){
+	
+}*/
 
 //Call on('submit') on the FORM not the submit button or ID
 //You are submitting a form, not a button
 $('form').on('submit', function(event) {
   event.preventDefault();
-	var $guess = $('#guess').val();
+
+	var $guess = parseInt( ($('#guess').val()), 10 );
+
+  $('#guess').val('');
 
 	if($guess < secretNumber){
 		$('.messages p').html(guessTooLow);
@@ -15,11 +23,15 @@ $('form').on('submit', function(event) {
 	else if($guess > secretNumber) {
 		$('.messages p').html(guessTooHigh);
 	}
-	else {
+	else if($guess === secretNumber){
 		$('.messages p').html(guessCorrect);
 	}
-	$('.guesses ul').append('<li>' + $guess + '</li>');
-})
+	if(parseInt($guess, 10) > 0 && parseInt($guess, 10) < 101 ){
+  	$('.guesses ul').append('<li>' + $guess + '</li>');
+  } else {
+		$('.messages p').html(notANumber);
+	}
+});
 
 
 
